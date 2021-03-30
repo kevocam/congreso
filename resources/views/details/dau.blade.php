23bb9b952a0f381a15243035d569e75a81f1fdff
kevocam.seo@gmail.com <kevocam.seo@gmail.com>
2021-03-29 19:05:38 -0500

    mobile2

--- resources/views/details/dau.blade.php
+++ resources/views/details/dau.blade.php
@@ -4,7 +4,7 @@
 
 {{-- <link rel="stylesheet" href="./css/details-dau.css" >
 <link rel="stylesheet" href="./css/details-dau-xl.css" media="(min-width:1024px)"> --}}
-<link rel="stylesheet" href="./css/details-dau-sm.css" media="(max-width: 780px)">
+<link rel="stylesheet" href="./css/details-dau-sm.css" {{-- media="(max-width: 780px)" --}}>
 
 
 
