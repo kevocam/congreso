18c04e7ce3565d1088231b8f6835e44ce9e6cf12
kevocam.seo@gmail.com <kevocam.seo@gmail.com>
2021-03-30 10:50:38 -0500

    mobile animation

--- resources/views/details/dau.blade.php
+++ resources/views/details/dau.blade.php
@@ -2,17 +2,15 @@
 
 @section('style')
 
-{{-- <link rel="stylesheet" href="./css/details-dau.css" >
-<link rel="stylesheet" href="./css/details-dau-xl.css" media="(min-width:1024px)"> --}}
 <link rel="stylesheet" href="./css/details-dau-sm.css" {{-- media="(max-width: 780px)" --}}>
+<link rel="stylesheet" href="./css/details-dau.css" media="(min-width:930px)" >
 
+<link rel="stylesheet" href="./css/details-dau-xl.css" media="(min-width:1100px)">  
 
 
 @endsection
-@section('content')
+@section('content')  
 
-  
-   
-    @livewire('details.dau')
+@livewire('details.dau')
     
 @endsection
--- resources/views/livewire/details/dau.blade.php
+++ resources/views/livewire/details/dau.blade.php
@@ -1,5 +1,5 @@
-<div class="dau">
 
+<div class="dau">
     <div class="dau__cartelLeft">
         <div class="dau__cartelLeft--item">
             <a href="https://wwww.facebook.com/dauperu" target="_blank">
@@ -51,8 +51,20 @@
         </x-slot>
     </x-jet-dialog-modal>
 {{--     <audio id="audio" src="./img/sound.mp3"></audio>
- --}}</div>
+ --}}
+    <div class="iconRotate" hidden>
+        <em>Gire el telefono</em>
+        <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M24 16C24 13.7909 25.7909 12 28 12H52C54.2091 12 56 13.7909 56 16V64C56 66.2091 54.2091 68 52 68H28C25.7909 68 24 66.2091 24 64V16Z" stroke="#096191" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M41 64H39" stroke="#096191" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M56 60H24" stroke="#096191" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M56 20H24" stroke="#096191" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M44 16H36" stroke="#096191" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+    </div>
+</div>
     
+
 <script>
 
     var audio = document.getElementById("audio");
